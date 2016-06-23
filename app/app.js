c0fd2010aeda66878cb89853f41710479d7f0d84
Matt Kistler <Matt.A.Kistler@gmail.com>
2016-06-22 20:24:37 -0500

    ajax skeleton

--- app/app.js
+++ app/app.js
@@ -16,15 +16,3 @@ App = Ember.Application.extend({
 loadInitializers(App, config.modulePrefix);
 
 export default App;
-
-// $('#import').on('click', function(){
-// 	alert('This will import ALL your Discogs items. You sure you want that?')
-// 	$.ajax({
-// 		url: 'https://api.discogs.com/users/sollix/collection/folders/0/releases?page=1&per_page=10',
-// 		method: 'GET',
-// 		dataType: 'jsonp',
-// 		success: function(response) {
-// 			console.log(response)
-// 		}
-// 	})
-// })
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,15 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+ajax: Ember.inject.service(),
 actions: {
 
-	
 		submitRelease() {
 			var artistText = this.get('artistText')
 			var titleText = this.get('titleText')
 			var labelText = this.get('labelText')
 			var year = this.get('year')
 			var formatText = this.get('formatText')
+			var notes = this.get('notesText')
 			var thumbURL = this.get('thumbURL')
 
 			var addRecord = this.store.createRecord('release', {
@@ -27,7 +28,20 @@ actions: {
 
 		deleteRelease(release) {
 			release.destroyRecord()
-		}
+		},
 
-	}
+		importCollection() {
+      		this.get('ajax').request('https://api.discogs.com/users/sollix/collection/folders/0/releases?page=1&per_page=10').then(function(response) {
+      			debugger
+      			var collection = []
+      			for (var i = 0; i < response.releases.length - 1; i++) {
+					collection.push({
+						artist: response.releases[i].basic_information.artists[0].name
+					})
+					// addRecord.save()
+				}
+			console.log(collection)
+      		})
+        }
+    }
 });
